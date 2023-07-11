b41b79fc4254536936cf8329b60e42bc7155bf3c
Guilherme <guilhermehf23@gmail.com>
2023-07-10 21:40:27 -0300

    Primeira Pagina HTML

---
+++ paginaInicio.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="pagInicioStyle.css">
+    <link rel="stylesheet" href="pagInicioStyleMenu.css">
+    <title>FGTec</title>
+</head>
+<body>
+
+<div id="container">
+
+    <header id="cab">
+
+    <img src="img/fgtec-logo-fundo-preto-1.svg" class="logoFgt">
+    <input type="text" name="pesquisa" placeholder="Pesquisar..." class="barraPesq" required/>
+    <img src="img/user.png" class="logoUser">
+
+
+    </header>
+
+    <!--Menu -->
+
+    <nav>
+                    <ul>
+                        <li><a href="#">Home</a>
+                            <ul>
+                                <li><a href="#">SiteMap</a></li>
+                                <li><a href="#">Livro do Marco</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Lancamento</a>
+                            <ul>
+                                <li><a href="#">2020</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Placa de Video</a>
+                            <ul>
+                                <li><a href="#">NVIDIA</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Computador</a>
+                            <ul>
+                                <li><a href="#">Notebook</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Hardware</a>
+                            <ul>
+                                <li><a href="#">Nóticias</a></li>
+                                <li><a href="#">Venda de Animes Completa</a></li>
+                                <li><a href="#">Vendas de Animes</a></li>
+                                <li><a href="#">Vendas de Light Novel</a></li>
+                                <li><a href="#">Vendas de Manga Semanal</a></li>
+                                <li><a href="#">Previsão de Vendas</a></li>
+                                <li><a href="#">Top Anuais</a></li>
+                            </ul>
+                        </li>
+                        
+
+                        
+
+                        
+                    </ul>
+ 
+
+     </nav>
+
+
+     <main id="pagPrincipal">
+        
+        <section id="gridUm">
+
+            <img src="img/PlacadeVideo1650.png" class="imgGrid">
+
+            <p class="fontTitulo">Placa de Vídeo Galax GeForce GTX 1650 Ex Plus 4GB GDDR6 - 65SQL8DS93E1 </p>
+
+            <p class="fontPreco">R$:1.091</p>
+
+            <input type="submit" name="benviar" value="Comprar" class="botaoC" />
+
+        </section>
+
+        <section id="gridDois">
+
+            <img src="img/PlacadeVideo1650.png" class="imgGrid">
+
+            <p class="fontTitulo">Placa de Vídeo Galax GeForce GTX 1650 Ex Plus 4GB GDDR6 - 65SQL8DS93E1 </p>
+
+            <p class="fontPreco">R$:1.091</p>
+
+            <input type="submit" name="benviar" value="Comprar" class="botaoC" />
+
+        </section>
+
+        <section id="gridTres">
+
+            <img src="img/PlacadeVideo1650.png" class="imgGrid">
+
+            <p class="fontTitulo">Placa de Vídeo Galax GeForce GTX 1650 Ex Plus 4GB GDDR6 - 65SQL8DS93E1 </p>
+
+            <p class="fontPreco">R$:1.091</p>
+
+            <input type="submit" name="benviar" value="Comprar" class="botaoC" />
+
+        </section>
+
+     </main>
+
+
+     <footer id="rodape">
+
+        <h2>Informacoes</h2>
+        
+
+     </footer>
+
+
+    
+
+   
+
+
+</div>
+    
+    
+</body>
+</html>
+
+
+
+
